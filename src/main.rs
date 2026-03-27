aed2ba7aef2b4289af11c9853790ec0d0f244872
Daniel Carrasco <1249741+Danixu@users.noreply.github.com>
2026-03-27 08:12:26 +0100

    Working copy version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Read, Seek};
+use std::io::{Read, Seek, Write};
 use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 
@@ -14,8 +14,8 @@ struct Block {
 fn main() {
     // Usamos sync_channel para poner un límite.
     // Si hay 100 bloques en la cola, el que intente enviar se pausará.
-    let (tx_in, rx_in) = mpsc::sync_channel::<Block>(100);
-    let (tx_out, rx_out) = mpsc::sync_channel::<Block>(100);
+    let (tx_in, rx_in) = mpsc::sync_channel::<Block>(16);
+    let (tx_out, rx_out) = mpsc::sync_channel::<Block>(16);
 
     // Como varios workers van a leer de rx_in, necesitamos protegerlo con un Mutex.
     // Arc (Atomic Reference Counting) nos permite compartirlo entre los hilos de forma segura.
@@ -41,11 +41,11 @@ fn main() {
                     Some(block) => {
                         // AQUÍ VA TU LÓGICA DE LZ4:
                         // let compressed = lz4::compress(&block.data);
-                        println!(
-                            "Processing the block {} with a size of {}",
-                            block.id,
-                            block.data.len()
-                        );
+                        //println!(
+                        //    "Processing the block {} with a size of {}",
+                        //    block.id,
+                        //    block.data.len()
+                        //);
                         let compressed_data = block.data;
 
                         // Enviamos el resultado a la cola de salida
@@ -68,29 +68,28 @@ fn main() {
 
     // 3. Iniciar el Productor (Hilo Lector)
     let reader_thread = thread::spawn(move || {
-        let file = File::open("test.bin").expect("There was an error reading the file");
+        let mut file = File::open("test.bin").expect("There was an error reading the file");
         let file_size = file
             .metadata()
             .expect("There was an error getting the file info")
             .len();
         println!("Tamaño total del archivo: {} bytes", file_size);
-        let mut buf_reader = BufReader::new(file);
         let mut read_bytes_left = file_size;
         let mut id = 0;
 
         while read_bytes_left > 0 {
-            let to_read: usize = cmp::min(read_bytes_left as usize, 2048);
+            let to_read: usize = cmp::min(read_bytes_left as usize, 2048000);
             let mut data = vec![0; to_read];
 
-            println!("Reading the {} block with a size of {}", id, to_read);
-            let _ = buf_reader.read_exact(&mut data);
+            //println!("Reading the {} block with a size of {}", id, to_read);
+            let _ = file.read_exact(&mut data);
 
             // Si la cola está llena (100 elementos), send() pausará este hilo automáticamente
             tx_in.send(Block { id, data }).unwrap();
 
             id += 1;
             read_bytes_left = file_size
-                - buf_reader
+                - file
                     .stream_position()
                     .expect("There was an erorr getting the stream pos");
         }
@@ -102,19 +101,24 @@ fn main() {
         // Búfer temporal para bloques que llegaron antes de tiempo (desordenados)
         let mut out_of_order_buffer: HashMap<usize, Vec<u8>> = HashMap::new();
 
+        let mut file =
+            File::create("test.out").expect("There was an error opening the output file");
+
         // El for loop sobre rx_out lee continuamente y se pausa si no hay datos.
         // Termina solo cuando todos los workers han cerrado su 'tx_out'.
         for block in rx_out {
             if block.id == expected_id {
                 // Es el bloque que esperábamos. Lo escribimos.
                 // AQUÍ VA TU LÓGICA DE ESCRITURA EN DISCO
-                println!("Escribiendo bloque a disco: {}", expected_id);
+                //println!("Escribiendo bloque a disco: {}", expected_id);
+                let _ = file.write_all(&block.data);
                 expected_id += 1;
 
                 // Ahora comprobamos si los siguientes bloques ya estaban esperando en el búfer
                 while let Some(buffered_data) = out_of_order_buffer.remove(&expected_id) {
                     // AQUÍ VA TU LÓGICA DE ESCRITURA EN DISCO (del dato guardado en memoria)
-                    println!("Escribiendo bloque desde memoria: {}", expected_id);
+                    //println!("Escribiendo bloque desde memoria: {}", expected_id);
+                    let _ = file.write_all(&buffered_data);
                     expected_id += 1;
                 }
             } else {
